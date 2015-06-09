0c9fe97c7ae452baf523b3d8c5d4fbf20650eeee
William Speirs <bill.speirs@gmail.com>
2015-06-09 15:11:58 -0400

    Removed incorrect statement about loops in Rust

--- code/rust/loops.rs
+++ code/rust/loops.rs
@@ -1,24 +1,28 @@
 fn main() {
 // BEGIN_CODE
-	let items = [1, 2, 3];
+    let items = [1, 2, 3];
 
-	for i in 0..items.len() {
-		println!("{}", items[i]);
-	}
+    for i in 0..items.len() {
+        println!("{}", items[i]);
+    }
+    
+    for i in &items {
+        println!("{}", i);
+    }
 
-	let mut i = 0;
+    let mut i = 0;
 
-	loop {
-		println!("{}", items[i]);
-                
-                i += 1;
-                
-		if i == items.len() {
-			break;
-                }
-	}
+    loop {
+        println!("{}", items[i]);
+        
+        i += 1;
+        
+        if i == items.len() {
+            break;
+        }
+    }
 
-	// Rust does not have do-while loops
+    // Rust does not have do-while loops
 // END_CODE
 }
 
